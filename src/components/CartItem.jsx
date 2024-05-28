5accb954d606ff0508c339290059a41a785e4a82
GaboSnipe <gabriel.gulua@geolab.edu.ge>
2024-05-29 00:45:33 +0400

    fix

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -44,7 +44,7 @@ const CartItem = ({ cartItem }) => {
     >
       {/* IMAGE */}
       <img
-        src={`process.env.REACT_APP_API_URL${image}`}
+        src={`${process.env.REACT_APP_API_URL}${image}`}
         alt={title}
         className="h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover"
       />
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -36,7 +36,7 @@ const Landing = () => {
                 key={product._id}
                 id={product._id}
                 title={product.name}
-                image={`process.env.REACT_APP_API_URL${product.imageUrl}`}
+                image={`${process.env.REACT_APP_API_URL}${product.imageUrl}`}
                 price={product.price}
               />
             ))}
